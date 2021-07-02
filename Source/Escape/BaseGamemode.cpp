6fb50e9e7b3a7fe86eba81c4a3408a5f4b159460
DivadTenni <davidctenni@gmail.com>
2021-07-02 16:52:29 +1000

    Added:  C++ Base Gamemode

---
+++ Source/Escape/BaseGamemode.cpp
@@ -0,0 +1,5 @@
+// tutorial said to fill out copyright section so i guess copyright david tenni 2021 although can you copyright code from a tutorial probs not idek whatever its done now
+
+
+#include "BaseGamemode.h"
+
---
+++ Source/Escape/BaseGamemode.h
@@ -0,0 +1,17 @@
+// tutorial said to fill out copyright section so i guess copyright david tenni 2021 although can you copyright code from a tutorial probs not idek whatever its done now
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaseGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API ABaseGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
